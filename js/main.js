e5712e6e2b2259fb3c3f0447b9559e23debbe68c
Stephaniechan2005 <stephanie_chan2005@yahoo.com.hk>
2024-10-31 23:47:56 -0400

    Added earbud model
    In this commit the earbud model added.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,62 @@
 (() => {
-  console.log("IIFE Fired");
-
-  // Variable
+  //variables
+  const model = document.querySelector("#model");
   const hotspots = document.querySelectorAll(".Hotspot");
-  //console.log(hotspots);
 
-  //functions
-  function showInfo(e) {
-    console.log(e.currentTarget.slot);
-    let selected = document.querySelector(
-      `button[slot="${e.currentTarget.slot}"] > div`
-    );
-    //button[slot="hotspot-4"> div]
+  //data
+
+  const infoBoxes = [
+    {
+      titles: "Noise cancelling microphone",
+      text: "Noise Cancelling microphone make the sound better",
+    },
+    {
+      titles: "Gel tip ear piece",
+      text: "the most comfortable ear piece ever",
+      image: "images/earbud.jpg",
+    },
+  ];
+
+  function loadInfo() {
+    infoBoxes.forEach((infoBox, index) => {
+      let selected = document.querySelector(`#hotspot-${index + 1}`);
+
+      let title = document.createElement("h2");
+      title.textContent = infoBox.title;
+      // create title variable = document.createElement("h2");
+      // title variable.textContent = infoBox.title;
+
+      let p = document.createElement("p");
+      p.textContent = infoBox.text;
+      //create a p variable = document.createElement("p");
+      //p variable.textContent = infoBox.text;
+
+      //selected.appendChild(title variable name);
+      //selected.appendChild( p variable name);
+      selected.appendChild(title);
+      selected.appendChild(p);
+    });
+  }
+
+  function showInfo() {
+    let selected = document.querySelector(`#${this.slot}`);
     gsap.to(selected, 1, { autoAlpha: 1 });
   }
-  function hideInfo(e) {
-    console.log("hideInfo called");
-    let selected = document.querySelector(
-      `button[slot="${e.currentTarget.slot}"] > div`
-    );
-    //button[slot="hotspot-4"> div]
-    gsap.to(selected, 1, { autoAlpha: 0 });
 
-    let name = "Marco";
-    console.log("Hello my name is" + name + "DeLuca");
-    console.log(`Hello my name
-      is ${name}
-      De Luca`);
+  function hideInfo() {
+    let selected = document.querySelector(`#${this.slot}`);
+    gsap.to(selected, 1, { autoAlpha: 0 });
   }
-  //event listeners
-  hotspots.forEach((hotspot) => {
-    hotspot.addEventListener("mouseover", showInfo);
-    hotspot.addEventListener("mouseout", hideInfo);
+
+  function modelLoaded() {
+    loadInfo();
+  }
+
+  //Event listeners
+  model.addEventListener("load", modelLoaded);
+
+  hotspots.forEach(function (hotspot) {
+    hotspot.addEventListener("mouseenter", showInfo);
+    hotspot.addEventListener("mouseleave", hideInfo);
   });
 })();
